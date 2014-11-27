f30ea2595682cd0858265248d8d84368620ab902
Matheus Graciano (pc-beta) <graciano.dev@gmail.com>
2014-11-27 07:38:09 -0200

    bug fix de re-instalação e mudanças específicas pro meu PC

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 FLAG_ZUERA="# LET THE ZUERA BEGIN"
 DEFINITIONS_FILE="bash_definitions"
+DEFINITIONS_FILE_ALTERNATIVE="bash_definitions_alternative"
 echo "========================================================================"
 echo "| This is the pograma Terminal da zuera, by graciano gracilindo        |"
 echo "| É open source: http://github.com/graciano/terminal-da-zuera          |"
@@ -14,20 +15,39 @@ echo "|______________________________________________________________________|"
 clear_previous_instalation(){
     NUMBER=$(awk "/$FLAG_ZUERA/{ print NR; exit }" ~/.bashrc)
     echo "$NUMBER"
-    echo "$TOTAL_OF_LINES"
     if [ -z "$NUMBER" ]
     then
 	    echo "Nenhuma instalação encontrada, prosseguindo..."
     else
 	    echo "Instalação encontrada, removendo conteúdo do arquivo $DEFINITIONS_FILE a partir da linha $NUMBER"
-	    sed -n '1,/$FLAG_ZUERA/q;p' ~/.bashrc
+	    sed -ni "/^$FLAG_ZUERA$/q;p" ~/.bashrc
     fi
 }
 
 install_zuera(){
-	cat "$DEFINITIONS_FILE" >> ~/.bashrc
+    clear_previous_instalation
+    echo "Você usa a pasta /var/www/html?"
+    read -p "Caso não, o programa assume que seja a /var/www/ " choice
+    if [[ $choice =~ ^[Yy]$ ]]
+    then
+         cat "$DEFINITIONS_FILE" >> ~/.bashrc
+    else
+        cat "$DEFINITIONS_FILE_ALTERNATIVE" >> ~/.bashrc
+    fi
+
+    if [ "$HOSTNAME"="beta-matheus" ]
+    then
+        cat graciano_definitions >> ~/.bashrc
+    fi
 	echo "Se você não viu nehuma mensagem de erro de permissão de escrita etc, tá tudo done"
 }
 
-clear_previous_instalation
-install_zuera
+tem_certeza(){
+    read -p "Tem certeza?" choice
+    if [[ $choice =~ ^[Yy]$ ]]
+    then
+         install_zuera
+    fi
+}
+
+tem_certeza
