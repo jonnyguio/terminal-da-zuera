b45fd8094dc33a379d830653769407407565c395
Matheus Graciano (pc-beta) <graciano.dev@gmail.com>
2015-09-01 22:30:49 -0300

    adicionando pulseaudio no bach_alternative.sh

--- bash_definitions_alternative.sh
+++ bash_definitions_alternative.sh
@@ -12,13 +12,16 @@ alias ..='cd ..'
 alias ...='cd ../../'
 alias tgz='tar -zxvf'
 alias rm='rm -i'  # confirmação antes de deletar um arquivo
-alias pulseaudio-start='pulseaudio --start'
 
 #   functions for complicated aliases
 function_kill_pulseaudio() {
     echo autospawn = no > $HOME/.config/pulse/client.conf
-    pulseaudio --kill
+    pkill -f pulseaudio
+}
+
+function_start_pulseaudio() {
     command rm "$HOME/.config/pulse/client.conf"
+    pulseaudio --start
 }
 
 function_show_webdev_folders() {
@@ -46,6 +49,7 @@ function_webdev() {
 alias web-dev=function_webdev
 alias webdev=function_webdev
 alias pulseaudio-kill=function_kill_pulseaudio
+alias pulseaudio-start=function_start_pulseaudio
 
 #   autocomplete for webdev is in ~/.bash_completion.d/webdev
 _webdev()
